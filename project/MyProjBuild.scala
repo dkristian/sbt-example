e1343469ae7e15f00fa483cc7bc6a56c88f8d2ac
kristian <kristian@rememberthemilk.com>
2011-08-31 11:30:11 +1000

    Move scalatest dep into full config

--- project/MyProjBuild.scala
+++ project/MyProjBuild.scala
@@ -8,4 +8,7 @@ object MyProjBuild extends Build {
       .configs( IntegrationTest )
       .settings( Defaults.itSettings : _*)
       .settings( watchSources <++= (unmanagedSources in IntegrationTest).identity )
+      .settings( libraryDependencies += scalatest )
+
+  lazy val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.4.1" % "test;it"
 }
